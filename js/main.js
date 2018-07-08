09eb555e4894b17a9acbfba6da5761336ba74d99
unknown <jskyers@spartaglobal.com>
2018-07-08 15:32:05 +0100

    adjustments to visuals for health bar, background and projectiles

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// let player1health = document.getElementById("player1health")
+// player1health.value -= 10;
+// let player2health = document.getElementById("player2health")
+// player2health.value -= 20;
+// let player3health = document.getElementById("player3health")
+// player3health.value -= 30;
+// let player4health = document.getElementById("player4health")
+// player4health.value -= 40;
