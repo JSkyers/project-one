1be05efe4512bdc3edf8ee96c8b5ecf3f3bf33d5
unknown <jskyers@spartaglobal.com>
2018-07-09 12:29:18 +0100

    round number changes accordingly

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var player3heavychance = 50;
 var player4lightchance = 80;
 var player4heavychance = 50;
 var playerturn = 1;
+var currentround = 1;
 
 
 
@@ -17,14 +18,20 @@ $(".players").click(function () {
           parseInt($("#player1health").attr("value"))
           $("#player"+$(this).attr("id")+"health")[0].value -= 10;
           if (playerturn == 4) {
-            playerturn = 1
+            playerturn = 1;
+            currentround += 1;
           } else playerturn += 1;
         }
         currentTurn();
+        currentRound();
     });
 
 function currentTurn() {
   $(".characterturntext").attr("src","images/player-"+playerturn+"-logo.png");
+}
+
+function currentRound() {
+  $(".currentroundtext").attr("src","images/round-"+currentround+".png");
 }
 
 
